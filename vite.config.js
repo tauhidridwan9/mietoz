2a92e2ebb1352b062f16b99495735071d9dead45
tauhidridwan9 <tauhidridwan9@gmail.com>
2024-09-29 21:29:34 +0700

    bla

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     base: '/build/',  // Base URL untuk asset yang dihasilkan
     build: {
         outDir: 'public/build',  // Path output yang benar
-        manifest: true,
+        manifest: true,          // Generate manifest untuk assets
         rollupOptions: {
-            input: 'resources/js/app.js',  // Entry point untuk build
-        }
-    }
+            input: ['resources/js/app.js', 'resources/css/app.css'], // Input file JS dan CSS
+        },
+    },
+    plugins: [
+        laravel({
+            input: ['resources/js/app.js', 'resources/css/app.css'], // Input file CSS dan JS
+            refresh: true,  // Auto-refresh saat file diubah
+        }),
+    ],
 });
