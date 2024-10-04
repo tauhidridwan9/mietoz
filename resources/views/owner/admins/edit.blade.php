422d0d79214884e64a716f6b91746a17d9fcb392
tauhidridwan9 <tauhidridwan9@gmail.com>
2024-10-04 23:42:21 +0700

    resolving avatar

--- resources/views/owner/admins/edit.blade.php
+++ resources/views/owner/admins/edit.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <h1>Edit Cashier</h1>
 
-    <form action="{{ route('owner.admins.update', $admin->id) }}" method="POST">
+    <form action="{{ route('owner.admins.update', $admin->id) }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
 
@@ -27,6 +27,18 @@
             <label for="password_confirmation" class="form-label">Confirm Password</label>
             <input type="password" name="password_confirmation" class="form-control" id="password_confirmation">
         </div>
+
+        <div class="mb-3">
+            <label for="profile_pictures" class="form-label">Profile Photo</label>
+            <input type="file" name="profile_pictures" class="form-control" id="profile_pictures">
+        </div>
+
+        @if ($admin->profile_pictures)
+        <div class="mb-3">
+            <label for="current_profile_picture" class="form-label">Current Profile Picture</label>
+            <img src="{{ asset('storage/' . $admin->profile_pictures) }}" alt="Profile Picture" class="img-thumbnail" width="150">
+        </div>
+        @endif
 
         <button type="submit" class="btn btn-primary">Update Admin</button>
     </form>
